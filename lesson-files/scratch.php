0dec3f8f17abc4d68e0e75f6ff2ea07e47ebb280
Joe Casabona <jcasabona@gmail.com>
2020-07-10 16:07:53 -0400

    debugging

--- lesson-files/scratch.php
+++ lesson-files/scratch.php
@@ -1,7 +1,17 @@
 <?php
-ini_set( 'error_reporting', 0 );
-if ( file_exists('inc/variables.php') ) {
-    require_once( 'inc/variables.php' ); 
+error_reporting(E_ALL);
+$a = 2;
+
+if( ( $a < 2 ) )  {
+    echo "Hello!";
 }
 
-echo $title;
+//set_cookie( 'error', true );
+?>
+
+<h1>Hello!</h1>
+<?php
+print_r( $a ); 
+$colors = array('red', 'blue', 'green');
+
+var_dump( $colors );
